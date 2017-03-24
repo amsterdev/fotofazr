5f333ea2669b94d9961af0319eefba42f83dc6de
amsterdev <twisteramazon@yahoo.com>
2017-03-24 23:09:05 +0100

    some clean up

--- js/photo.js
+++ js/photo.js
@@ -3,37 +3,24 @@
 
 (function (w){
 	var dbody = document.querySelectorAll("body")[0];
-		console.log('what theee', dbody);
-//	dbody.className = ' blaze'
 
-
-	var photoWrap = document.querySelector('.photo_div_wrap');
-	console.log(photoWrap)
-	//dbody.addEventListener('scroll', function(){
-		console.log('what time is it')
-	//})
-
-
-
-
+	var photoWrap = document.querySelector('.photo_div_mask');
 
 	var last_known_scroll_position = 0;
 	var ticking = false;
 
-	var doSomething = function(){
-		var rawNumber = last_known_scroll_position * .15;
-		rawNumber = Math.round(rawNumber)
-//		console.log('raw is ', rawNumber,photoWrap.style.opacity);
+	var photoMaskUpdate = function(){
+		var rawNumber = last_known_scroll_position * .1;
+		rawNumber = Math.round(rawNumber);
 		photoWrap.style.opacity =  (rawNumber * .01);
 	}
 
-	window.addEventListener('scroll', function(e) {
+	w.addEventListener('scroll', function(e) {
 	  last_known_scroll_position = window.scrollY;
 	  if (!ticking) {
 	    window.requestAnimationFrame(function() {
-	      doSomething(last_known_scroll_position);
+	      photoMaskUpdate(last_known_scroll_position);
 	      ticking = false;
-	      console.log(last_known_scroll_position)
 	    });
 	  }
 	  ticking = true;
